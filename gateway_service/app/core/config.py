cec5dca5d378866f43fd4c9f06f6dee002433b4d
Nikita.Pavlichenko <nikita.pavlichenko@jetbrains.com>
2024-08-14 00:17:14 +0200

    Some fixes

---
+++ gateway_service/app/core/config.py
@@ -0,0 +1,6 @@
+import os
+
+DATABASE_URL = "postgresql://user:password@postgresdb/dbname"
+ADMIN_TOKEN = os.getenv("ADMIN_KEY")
+RATE_LIMIT = 5  # max requests per minute
+RATE_LIMIT_KEY_PREFIX = "rate_limit"
--- middleware_service/app/core/config.py
+++
@@ -1,4 +0,0 @@
-DATABASE_URL = "postgresql://user:password@postgresdb/dbname"
-ADMIN_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoicm9vdCIsImV4cCI6MTc1OTE5ODkxMSwiaWF0IjoxNzIzMTk4OTExfQ.Es-7-MMT5LPP4kW_L-MZxQ4h1pYC29HjNMzx0cYCkgk"
-RATE_LIMIT = 5  # max requests per minute
-RATE_LIMIT_KEY_PREFIX = "rate_limit"
